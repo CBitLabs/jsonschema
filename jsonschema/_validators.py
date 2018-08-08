a761bfd685b2272839440483c11e7875616cb1fd
Zac <zdelagrange@bitsighttech.com>
2018-08-08 14:36:40 -0400

    add x-nullable check

--- jsonschema/_validators.py
+++ jsonschema/_validators.py
@@ -385,13 +385,11 @@ def extends_draft3(validator, extends, instance, schema):
 def type(validator, types, instance, schema):
     types = _utils.ensure_list(types)
 
-    is_correct_type = any(validator.is_type(instance, type) for type in types)
-    is_null = instance is None
-    is_nullable = 'x-nullable' in schema and schema['x-nullable'] == True and is_null
-
-    # if not any(validator.is_type(instance, type) for type in types):
-    if not is_correct_type or not is_nullable:
-        yield ValidationError(_utils.types_msg(instance, types))
+    if not any(validator.is_type(instance, type) for type in types):
+        if 'x-nullable' in schema and schema['x-nullable'] == True and instance == None:
+            return
+        else:
+            yield ValidationError(_utils.types_msg(instance, types))
 
 
 def properties(validator, properties, instance, schema):
